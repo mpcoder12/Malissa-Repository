c216857a9d013067fc49f08fadb15f1bd5b06ef7
mpcoder12 <31974647+mpcoder12@users.noreply.github.com>
2017-11-24 20:59:47 -0500

    Final Project for CMPT 120
    This is my final project for Marist CMPT 120. There are no errors when running program through W3 Validator or in the console. 
    Enjoy this text adventure!

---
+++ locations.js
@@ -0,0 +1,54 @@
+function start(){
+	var description = "You find yourself located in a vast desert alone and you need to find your way out. All you have is water and some leftover food. Type a direction or click a button below. Type help for instructions.";
+	return description;	
+}
+
+function sandDunes(){
+	var description = "You begin to walk North and encounter sand dunes. You lie down in the sand. this is a good spot to look around";
+	return description;
+}
+
+function cacti(){
+	var description = "You see some cacti in the distance. This may be an opportunity to get more water. Should you try your luck?";
+	return description;
+}
+
+function caravanApproach(){
+	var description = "You reach the cacti. There is a caravan approaching. They have already seen you.";
+	return description;	
+}
+
+function caravanHostage(){
+	var description = "They already view you as hostile and take you hostage. You manage to steal a small knife off of one of your captors during the kidnapping. Take knife?";
+	return description;
+}
+
+function caravanCut(){
+	var description = "You begin to use the small knife to cut away at the ropes that bind you. Once free, you spot a gun in the caravan cart. Take gun?";
+	return description;
+}
+
+function caravanEscape(){
+	var description = "Once free you remove the covering from your face and smell smoke. You jump out ready to fight.";
+	return description;
+}
+
+function caravanFight(){
+	var description = "You manage to disarm all of the men in the caravan without killing them. They have brought you to your plane crash site. Your crew is still alive inside. Take crew?";
+	return description;
+}
+
+function planeCrashSite(){
+	var description = "You manage to save your crew from the burning wreckage. They explain that before the airplane caught fire, they sent the caravan to find you.";
+	return description;
+}
+
+function caravanHelp(){
+	var description = "You and your crew return to where you left the caravan. They are recovering from the beat-down you gave them. You apologize. They accept and bring you back to their village.";
+	return description;
+}
+
+function ending(){
+	var description = "The villagers have prepared a feast for everyone's return. You may not be home but this is home now.";
+	return description;
+}
